773602ba7e6ad7d724f3010325b0b09ea0ba5aa2
sassela <sassela@users.noreply.github.com>
2023-09-01 13:29:52 +0100

    add api endpoint

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api", (_req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
