8f95fe8b573048c2227f0bdf78022b8cf5e28c3e
sassela <sassela@users.noreply.github.com>
2023-09-07 09:05:53 +0100

    factor out Loading component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,12 @@
-import React from "react";
 import "./App.css";
 import TransactionPage from "./pages/TransactionPage";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         Fraud Detection App
-        <p>{!data ? "Loading..." : data}</p>
       </header>
       <TransactionPage/>
     </div>
