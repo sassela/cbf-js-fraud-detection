acc549635cd64294087c8556bf1483e17f017b69
sassela <sassela@users.noreply.github.com>
2023-09-15 06:30:27 +0100

    server/index.js -> server.index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,39 +1,42 @@
-require('dotenv').config();
-const path = require('path');
-const express = require("express");
-const { Client } = require('@elastic/elasticsearch')
+import dotenv from 'dotenv';
+import path from 'path';
+import express, { Request, Response } from 'express';
+import { Client } from '@elastic/elasticsearch';
 
-const PORT = process.env.PORT || 3001;
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 3001;
 
 const app = express();
 const client = new Client({
   cloud: {
-    id: process.env.ELASTIC_CLOUD_ID
+    id: process.env.ELASTIC_CLOUD_ID!,
   },
   auth: {
-    username: process.env.ELASTIC_AUTH_USERNAME,
-    password: process.env.ELASTIC_AUTH_PASSWORD
-  }
-})
+    username: process.env.ELASTIC_AUTH_USERNAME!,
+    password: process.env.ELASTIC_AUTH_PASSWORD!,
+  },
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
+app.get("/api", (req: Request, res: Response) => {
   res.json({ message: "Hello from server!" });
 });
 
 // Handle GET requests to /search route
-app.get("/search", async (req, res) => {
-  const es_index = "ulb_credit_card_fraud";
+app.get("/search", async (req: Request, res: Response) => {
+  const es_index: string = "ulb_credit_card_fraud";
 
   try {
-    body = await client.search({
+    // const { body } = await client.search({
+    const response = await client.search({
       index: es_index,
     });
 
-    res.json(body);
+    res.json(response);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error." });
@@ -41,7 +44,7 @@ app.get("/search", async (req, res) => {
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
