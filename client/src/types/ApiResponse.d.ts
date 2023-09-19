ece765a60c250f6a02fe2167a83c8c31f8d0b867
sassela <sassela@users.noreply.github.com>
2023-09-19 05:38:38 +0100

    add API response and Hit data types

---
+++ client/src/types/ApiResponse.d.ts
@@ -0,0 +1,20 @@
+import { Hit } from './Hit'
+
+export interface ApiResponse {
+  took: number;
+  timed_out: boolean;
+  _shards: {
+    total: number;
+    successful: number;
+    skipped: number;
+    failed: number;
+  };
+  hits: {
+    total: {
+      value: number;
+      relation: string;
+    };
+    max_score: number;
+    hits: Hit[];
+  };
+}
