ce424b8cab444d299821a155254fe8e484e3edea
sassela <sassela@users.noreply.github.com>
2023-09-19 05:44:28 +0100

    update Transaction type to the new response from elasticsearch

--- client/src/types/Hit.d.ts
+++ client/src/types/Hit.d.ts
@@ -1,38 +1,8 @@
+import { Transaction } from "./Transaction";
+
 export interface Hit {
   _index: string;
   _id: string;
   _score: number;
-  _source: {
-    Time: number;
-    V21: number;
-    V20: number;
-    V23: number;
-    V22: number;
-    V25: number;
-    V24: number;
-    V27: number;
-    V1: number;
-    V26: number;
-    V2: number;
-    V3: number;
-    V28: number;
-    V4: number;
-    V5: number;
-    V6: number;
-    V7: number;
-    V8: number;
-    V9: number;
-    Amount: number;
-    V10: number;
-    V12: number;
-    V11: number;
-    V14: number;
-    V13: number;
-    V16: number;
-    Class: number;
-    V15: number;
-    V18: number;
-    V17: number;
-    V19: number;
-  };
+  _source: Transaction
 }
